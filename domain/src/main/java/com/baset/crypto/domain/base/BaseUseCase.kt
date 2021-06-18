a6c5de0baaf1f7f95550877a717aedaec330fb86
Baset <b.esmaeili@parsdata.com>
2021-06-18 14:04:33 +0430

    Some Improvements on BaseUseCase

--- domain/src/main/java/com/baset/crypto/domain/base/BaseUseCase.kt
+++ domain/src/main/java/com/baset/crypto/domain/base/BaseUseCase.kt
@@ -1,17 +1,12 @@
 package com.baset.crypto.domain.base
 
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.flow.Flow
-
 /**
  * a Contract for create UseCases
  */
-abstract class BaseUseCase<out T, in Params> {
-    protected abstract fun createFlow(
-        params: Params? = null,
-        dispatcher: CoroutineDispatcher
-    ): Flow<T>
+abstract class BaseUseCase<out Result, in Params> {
+    protected abstract suspend fun create(
+        params: Params? = null
+    ): Result
 
-    operator fun invoke(params: Params?, dispatcher: CoroutineDispatcher) =
-        createFlow(params, dispatcher)
+    suspend operator fun invoke(params: Params?) = create(params)
 }
